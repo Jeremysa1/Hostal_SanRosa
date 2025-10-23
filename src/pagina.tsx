5a369aeba88b4483a3b76da8b88ea92c3c7f0a29
camilavm9 <camilavm9@gmail.com>
2025-10-23 00:58:14 +0000

    sitios turisticos otra vez

--- src/pagina.tsx
+++ src/pagina.tsx
@@ -9,17 +9,16 @@ const Pagina = () => {
                 <img src="https://res.cloudinary.com/dfznn7pui/image/upload/v1761079649/termales-santa-2_wzlgap.jpg " alt="Termales Santa Rosa" />
             </div>
             <div className="titulo">
-               <strong> <h1>TERMALES</h1>
-                <h2 className='subtitulo'>Santa Rosa de Cabal</h2></strong>
+                <strong> <h1>TERMALES</h1>
+                    <h2 className='subtitulo'>Santa Rosa de Cabal</h2></strong>
             </div>
             <div className='texto'>
                 <p>
-                Son uno de los lugares más reconocidos del Eje Cafetero, ideales para disfrutar de un descanso total en medio de la naturaleza. Sus aguas termales brotan de la montaña y forman cascadas que crean un paisaje único y relajante. El sonido del agua y el aire fresco de las montañas hacen de este sitio un refugio perfecto para desconectarse del ritmo diario. Además, cuenta con zonas de spa, hotel y senderos ecológicos que completan una experiencia inolvidable.
-   
+                    Son uno de los lugares más reconocidos del Eje Cafetero, ideales para disfrutar de un descanso total en medio de la naturaleza. Sus aguas termales brotan de la montaña y forman cascadas que crean un paisaje único y relajante. El sonido del agua y el aire fresco de las montañas hacen de este sitio un refugio perfecto para desconectarse del ritmo diario. Además, cuenta con zonas de spa, hotel y senderos ecológicos que completan una experiencia inolvidable.
                 </p>
                 <button className="boton">RESERVA</button>
             </div>
-            
+
 
         </>
     );
