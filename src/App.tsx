2cdcb6412e7d18b6213042a68fcf5e08522568b1
laura20010905 <laura20010905@gmail.com>
2025-10-25 00:17:45 +0000

    Inicio con carrusel

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import Menu from './componentes/Menu';
 import Descripcion from './componentes/Descripcion';
 import Footer from './componentes/Footer';
 import Mapa from './componentes/Mapa';
+import CarruselInicio from './componentes/CarruselInicio';
 import './App.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+
 
 function App() {
   return (
     <div className="App">
       <Menu />
+      <CarruselInicio />
       <Descripcion />
       <FranjaIconos />
       <CarruselHabitaciones />
