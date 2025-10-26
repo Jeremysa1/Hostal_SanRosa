fa28a4cf9d701db9681eceb73821032e7c104db0
laurasofia1arias <laurasofia1arias@gmail.com>
2025-10-26 22:36:38 +0000

    saque el codigo de app.tsx a room.ts

---
+++ src/data/rooms.ts
@@ -0,0 +1,56 @@
+export const cardData = [
+      {
+        imageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760064533/habitacion_9_mhcbqn.png",
+        detailImageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760068145/pop_9_ilwxua.png",
+        title: "Habitación",
+        subtitle: "FAMILIAR",
+        description: "Perfecta para familias o grupos pequeños.",
+        rating: 3,
+        services: ["Wi-Fi", "Agua caliente"]
+      },
+      {
+        imageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760064532/habitacion_3_tyjjyj.png",
+        detailImageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760068140/pop_3_twmie0.png",
+        title: "Habitación",
+        subtitle: "DOBLE",
+        description: "Diseñada para parejas o viajeros que buscan comodidad.",
+        rating: 4,
+        services: ["Wi-Fi", "Agua caliente", "Televisión", "Mesa de trabajo"]
+      },
+      {
+        imageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760064532/habitacion_4_xxw1ne.png",
+        detailImageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760068141/pop_4_rmiegd.png",
+        title: "Habitación",
+        subtitle: "FAMILIAR",
+        description: "Amplia y cómoda, ideal para grupos grandes.",
+        rating: 2,
+        services: ["Wi-Fi", "Agua caliente", "Sofá cama", "Closet" ]
+      },
+      {
+        imageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760064532/habitacion_1_c2dcmu.png",
+        detailImageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760068139/pop_1_y8szeq.png",
+        title: "Habitación",
+        subtitle: "TRIPLE",
+        description: "Espacio funcional y tranquilo, ideal para amigos o familias pequeñas.",
+        rating: 3,
+        services: ["Wi-Fi", "Agua caliente" ]
+      },
+      {
+        imageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760064532/habitacion_5_jb3zxx.png",
+        detailImageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760068142/pop_6_oepcso.png", 
+        title: "Habitación",
+        subtitle: "DOBLE",
+        description: "Acogedora y práctica, perfecta para descansar después de un día de viaje.",
+        rating: 4,
+        services: ["Wi-Fi", "Agua caliente" ]
+      },
+      {
+        imageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760064533/habitacion_7_onpc4d.png",
+        detailImageUrl: "https://res.cloudinary.com/dfznn7pui/image/upload/v1760068143/pop_7_wnj3v0.png",
+        title: "Habitación",
+        subtitle: "SUITE",
+        description: "Nuestra habitación más completa, ideal para quienes buscan mayor confort.",
+        rating: 5,
+        services: ["Wi-Fi", "Sofá cama", "Agua caliente", "Repisa", "Televisión", "Mesa de trabajo" ]
+      }
+    ];
