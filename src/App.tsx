71e520faaa5350ff283afd66c3887b8f6f24cbe8
Sofia <laurasofia1arias@gmail.com>
2025-11-06 19:25:46 -0500

    cambios de isabella

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
-
-import { useState } from 'react';
+import React from 'react';
+import Tarjetas from './componentes/Tarjetas';
+import Menu from './componentes/Menu';
+import Footer from './componentes/Footer';
 import './App.css';
-import { CustomCard } from './componentes/CustomCard.tsx';
-import { RoomDetail } from './componentes/RoomDetail.tsx';
-import Menu from './componentes/Menu.tsx';
-import FranjaIconos from './componentes/FranjaIconos.tsx';
-import Footer from './componentes/Footer.tsx';
-import { cardData } from './componentes/infoRooms.tsx';
 
-function App() {
-    const [selectedRoom, setSelectedRoom] = useState<any>(null);
-
-    return (
-      <>
-        <Menu />
-        <FranjaIconos />
-        <div className="content-wrapper">
-          <div className="grid p-5">
-            {cardData.map((data, index) => (
-              <div key={index} className="col-12 md:col-6 lg:col-4">
-                <CustomCard {...data} onCardClick={() => setSelectedRoom(data)} />
-              </div>
-            ))
-
-            }
-
-            {selectedRoom && (
-              <RoomDetail room={selectedRoom} visible={selectedRoom !== null} onHide={() => setSelectedRoom(null)} />
-            )}
-          </div>
-        </div>
-        <Footer />
-      </>
-    );
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <Menu />
+      <Tarjetas />
+      <Footer />
+    </>
+  );
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
-import 'primeflex/primeflex.css';                                   // css utility
-import 'primeicons/primeicons.css';
-import 'primereact/resources/primereact.css'; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
